c0a031e007935c9cc23f5a46346f1df1c3da4f13
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-08-18 07:40:59 -0600

    chore: generate python samples templates in owlbot.py (#22)
    Generate python samples templates in owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -60,6 +60,7 @@ s.remove_staging_dirs()
 # ----------------------------------------------------------------------------
 
 templated_files = gcp.CommonTemplates().py_library(cov_level=99, microgenerator=True)
+python.py_samples(skip_readmes=True)
 s.move(templated_files, excludes=[".coveragerc"]) # the microgenerator has a good coveragerc file
 
 # ----------------------------------------------------------------------------
